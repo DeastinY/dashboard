f26279cb3fa4878f50c6db93c38e25f54b6da904
Richard Polzin <polzin.richard@gmail.com>
2016-09-02 08:40:48 +0200

    introduced ugly code duplication, but ON OFF work now

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,7 @@ b = Bridge('192.168.1.15')  # Enter IP here
 
 b.connect()
 groups = OrderedDict(sorted(b.get_group().items()))
+print(groups)
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,11 +22,29 @@ def hello_world():
 
 @app.route('/togglelight', methods=["POST"])
 def togglelamp():
-    light = int(request.get_json()['name'])
+    light = int(request.get_json()['name'][1:])
     on = b.get_light(light,'on')
     print("Turning light {} {}".format(light, "off" if on else "on"))
     b.set_light(light, 'on', not on)
     return jsonify(success = True, on = not on, light = light)
+
+@app.route('/lighton', methods=["POST"])
+def lighton():
+    groupid = int(request.get_json()['name'])
+    lights = b.get_group(groupid, 'lights')
+    lights = [int(l) for l in lights]
+    print("Turning lights {} on".format([l for l in lights]))
+    b.set_light(lights, 'on', True)
+    return jsonify(success = True, lights=lights)
+
+@app.route('/lightoff', methods=["POST"])
+def lightoff():
+    groupid = int(request.get_json()['name'])
+    lights = b.get_group(groupid, 'lights')
+    lights = [int(l) for l in lights]
+    print("Turning lights {} off".format([l for l in lights]))
+    b.set_light(lights, 'on', False)
+    return jsonify(success=True, lights=lights)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- static/js/custom.js
+++ static/js/custom.js
@@ -11,13 +11,56 @@ $(function() {
             success: function(response) {
                 console.log(response);
                 var name = response['light'];
-                var btn = $("button[name="+name+"]");
+                var btn = $("button[name=l"+name+"]");
                 btn.removeClass('btn-warning');
                 btn.removeClass('btn-default');
                 if (response['on']) {
                     btn.addClass('btn-warning');
                 } else {
                     btn.addClass('btn-default');
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+    $('.onbtn').click(function() {
+        $.ajax({
+            url: '/lighton',
+            type: 'POST',
+            contentType : 'application/json',
+            dataType : 'json',
+            data: JSON.stringify({
+                "name" : $(this).attr('name')
+            }),
+            success: function(response) {
+                console.log(response);
+                for (var l in response['lights']) {
+                    console.log(l);
+                    var btn = $("button[name=l"+response['lights'][l]+"]");
+                    btn.removeClass('btn-default').addClass('btn-warning');
+                }
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+    $('.offbtn').click(function() {
+        $.ajax({
+            url: '/lightoff',
+            type: 'POST',
+            contentType : 'application/json',
+            dataType : 'json',
+            data: JSON.stringify({
+                "name" : $(this).attr('name')
+            }),
+            success: function(response) {
+                console.log(response);
+                for (var l in response['lights']) {
+                    var btn = $("button[name=l"+response['lights'][l]+"]");
+                    btn.removeClass('btn-warning').addClass('btn-default');
                 }
             },
             error: function(error) {
